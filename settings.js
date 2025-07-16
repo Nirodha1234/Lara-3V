33f0479b60443822f88f329cdcfe2c815fe76659
Nirodha1234 <indikayapabanda@gmail.com>
2025-07-16 17:53:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "mQJR1YYY#-XkWNT3Y_pnok1Tc4mewh0smiDO6Qz8gszoQ4HKfLgU", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
